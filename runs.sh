eea4e043f355489a02e987b8877b5e0643e5b858
fergus <fergusfettes@gmail.com>
2024-02-19 15:46:11 +0530

    added dynamics

--- runs.sh
+++ runs.sh
@@ -4,7 +4,6 @@ python demo.py --cutoff 1e-6 --prompt "A typical definition of X would be '"
 python demo.py --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
 python demo.py --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
 
-
 python demo.py --model-name gptj --cutoff 1e-6 --prompt "A typical definition of X would be '"
 python demo.py --model-name gptj --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
 python demo.py --model-name gptj --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
