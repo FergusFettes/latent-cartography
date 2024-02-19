a9c43e8953158379ae8c1ec162baffbf4e36b245
fergus <fergusfettes@gmail.com>
2024-02-19 15:09:27 +0530

    added bash script for some runs

---
+++ runs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+python demo.py --cutoff 1e-6 --prompt "A typical definition of X would be '"
+python demo.py --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
+python demo.py --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
+
+
+python demo.py --model-name gptj --cutoff 1e-6 --prompt "A typical definition of X would be '"
+python demo.py --model-name gptj --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
+python demo.py --model-name gptj --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
+
+python demo.py --model-name llama --cutoff 1e-6 --prompt "A typical definition of X would be '"
+python demo.py --model-name llama --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
+python demo.py --model-name llama --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
+
+python demo.py --model-name mistral --cutoff 1e-6 --prompt "A typical definition of X would be '"
+python demo.py --model-name mistral --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
+python demo.py --model-name mistral --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
