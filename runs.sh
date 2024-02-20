3d05ac3b93a326cb552bb0b2470b7f6255b7a08e
fergus <fergusfettes@gmail.com>
2024-02-20 20:16:33 +0530

    updated run script

--- runs.sh
+++ runs.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-python demo.py --cutoff 1e-6 --prompt "A typical definition of X would be '"
-python demo.py --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
-python demo.py --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
+python cumulative_probability_expansion.py --cutoff 1e-6 --prompt "A typical definition of X would be '"
+python cumulative_probability_expansion.py --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
+python cumulative_probability_expansion.py --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
 
-python demo.py --model-name gptj --cutoff 1e-6 --prompt "A typical definition of X would be '"
-python demo.py --model-name gptj --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
-python demo.py --model-name gptj --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
+python cumulative_probability_expansion.py --model-name gptj --cutoff 1e-6 --prompt "A typical definition of X would be '"
+python cumulative_probability_expansion.py --model-name gptj --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
+python cumulative_probability_expansion.py --model-name gptj --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
 
-python demo.py --model-name llama --cutoff 1e-6 --prompt "A typical definition of X would be '"
-python demo.py --model-name llama --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
-python demo.py --model-name llama --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
+python cumulative_probability_expansion.py --model-name llama --cutoff 1e-6 --prompt "A typical definition of X would be '"
+python cumulative_probability_expansion.py --model-name llama --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
+python cumulative_probability_expansion.py --model-name llama --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
 
-python demo.py --model-name mistral --cutoff 1e-6 --prompt "A typical definition of X would be '"
-python demo.py --model-name mistral --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
-python demo.py --model-name mistral --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
+python cumulative_probability_expansion.py --model-name mistral --cutoff 1e-6 --prompt "A typical definition of X would be '"
+python cumulative_probability_expansion.py --model-name mistral --cutoff 1e-5 --prompt "A typical definition of X would be 'a woman"
+python cumulative_probability_expansion.py --model-name mistral --cutoff 1e-5 --prompt "A typical definition of X would be 'a man"
